4ccedc7355b8a66657895d317159e61e4e237e99
Christoph Brosch <christoph.brosch@outlook.de>
2021-08-07 18:50:41 +0200

    initial commit

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3vx*6+2@v(v!a@n57@n930xh!%2(*zwym_6ughme516&*3s1qi'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
